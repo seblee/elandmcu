7a3d56fa312f6369bd448f071de80a0cbc4473ff
seblee <ceeu@vip.qq.com>
2018-08-03 14:00:58 +0800

    set version v01.28

--- ELAND_OTA/eland_ota.h
+++ ELAND_OTA/eland_ota.h
@@ -17,7 +17,7 @@
 
 /* Private define ------------------------------------------------------------*/
 #define MCU_VERSION_MAJOR (uint8_t)1
-#define MCU_VERSION_MINOR (uint8_t)27
+#define MCU_VERSION_MINOR (uint8_t)28
 /* Private macro -------------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
